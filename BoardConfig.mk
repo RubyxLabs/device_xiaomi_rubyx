83045be036c69f54c1b3caaac15aebbc041c0665
Aeron-Aeron <aeronrules2@gmail.com>
2025-12-03 13:06:51 +0545

    rubyx: Drop inclusion of FCM from vendor/lineage
    * Every rom source is moving it to build from
      hardware/lineage/interfaces
    
    Signed-off-by: Aeron-Aeron <aeronrules2@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_SKUS += nfc
 DEVICE_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
